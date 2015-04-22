f16ee3a7dfaa0eaeadbff164be48fe1032fac498
Bridgit Donnelly <bridgit.donnelly@gmail.com>
2015-04-21 20:06:48 -0500

    Working pipeline. Many edits to come

--- pipeline.py
+++ pipeline.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import pylab
 import sys
 import sklearn as sk
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 # Code adapted from https://github.com/yhat/DataGotham2013/
 
@@ -27,21 +28,22 @@ class MLPipeline():
 
 	"""
 
-	def __init__(self, training_data = None, testing_data = None, methods = [], run_immediately = True):
+	def __init__(self, training_data = None, testing_data = None, run_immediately = True):
 		
 		if training_data:
 			self.training_data = training_data
 		if testing_data:
 			self.testing_data = testing_data
-		if methods:
-			self.methods = methods
-
+		
 		if run_immediately:
 			# Load Training Data
 			self.df = self.read_data(self.training_data)
 
 			# Output Training Summary Statistics
 			self.summary = self.summarize(self.df)
+
+			# Load Testing Data
+			self.test_df = self.read_data(self.testing_data)
 
 
 	## 1. Read Data: For this assignment, assume input is CSV
@@ -86,6 +88,18 @@ class MLPipeline():
 
 		df.to_csv(filename)
 
+	def histogram(df, field, bins):
+		'''Given a dataframe, a field and a bin count, this creates histograms.'''
+
+		fn = field + '.png'
+
+		#Create histogram
+		df[field].hist(xlabelsize=10, ylabelsize=10, bins=bins)
+		pylab.title("Distribution of {0}".format(field))
+		pylab.xlabel(field)
+		pylab.ylabel("Counts")
+		pylab.savefig(fn)
+		pylab.close()
 
 # ---------------------------------------------------------------------
 
@@ -114,7 +128,7 @@ class MLPipeline():
 		# Split data into test and train for cross validation
 		is_test = np.random.uniform(0, 1, len(df)) > 0.75
 		train = df[is_test==False]
-		test = df[is_test==True]
+		validate = df[is_test==True]
 		
 		## Calibrate imputation with training data
 		imputer = KNeighborsRegressor(n_neighbors=1)
@@ -131,13 +145,25 @@ class MLPipeline():
 		# Combine Training Data Back Together
 		train = train_not_null.append(train_null)
 
-		# Apply Nearest Neighbors to Testing Data
+		# Apply Nearest Neighbors to Validation Data
 		new_var_name = variable + 'Imputed'
-		test[new_var_name] = imputer.predict(test[cols])
-		test[variable] = np.where(test[variable].isnull(), test[new_var_name],
-									test[variable])
+		validate[new_var_name] = imputer.predict(validate[cols])
+		validate[variable] = np.where(validate[variable].isnull(), validate[new_var_name],
+									validate[variable])
 
-		return train, test
+
+		# Drop Imputation Column & Combine Test & Validation
+		validate.drop(new_var_name, axis=1, inplace=True)
+		df = train.append(validate)
+
+		''' FOR FUTURE
+		# Apply Imputation to Testing Data
+		test_df[new_var_name] = imputer.predict(test_df[cols])
+		test_df[variable] = np.where(test_df[variable].isnull(), test_df[new_var_name],
+									test_df[variable])
+		'''
+
+		return df.sort_index()
 		
 
 # ---------------------------------------------------------------------
@@ -145,6 +171,39 @@ class MLPipeline():
 	## 4. Generate Features: Write a sample function that can discretize a continuous variable
 	## and one function that can take a categorical variable and create binary variables from it.
 
+	def find_features(self, df, features, variable):
+		'''Uses random forest algorithm to determine the relative importance of each
+		potential feature. Takes dataframe, a numpy array of features, and the dependent
+		variable. Outputs dataframe, sorting features by importance'''
+		clf = RandomForestClassifier()
+		clf.fit(df[features], df[variable])
+		importances = clf.feature_importances_
+		sort_importances = np.argsort(importances)
+		rv = pd.DataFrame(data={'variable':features[sort_importances],
+								'importance':importances[sort_importances]})
+		return rv
+
+	def adjust_outliers(self, x, cap):
+		'''Takes series and creates upperbound cap to adjust for outliers'''
+		if x > cap:
+			return cap
+		else:
+			return x
+
+	def bin_variable(self, df, variable, num_bins, labels=None):
+		'''Discretizes a continuous variable based on specified number of bins'''
+		new_label = variable + '_bins'
+		df[new_label] = pd.cut(df[variable], bins=num_bins, labels=labels)
+
+	def get_dummys(self, df, cols):
+		'''Creates binary variable from specified column(s)'''
+		# Loop through each variable
+		for variable in cols:
+			dummy_data = pd.get_dummies(df[variable], prefix=variable)
+			
+			# Add new data to dataframe
+			df = pd.concat([df, dummy_data], axis=1)
+	
 
 # ---------------------------------------------------------------------
 
@@ -154,13 +213,24 @@ class MLPipeline():
 	## 6. Evaluate Classifier: you can use any metric you choose for this assignment (accuracy 
 	## is the easiest one)
 
+	def nearest_neighbors(self, df, features, variable):
+		'''Uses nearest neighbors to generate and evaluate predictions of a specified
+		variable.'''
+		
+		# Split data into test and train for cross validation
+		is_test = np.random.uniform(0, 1, len(df)) > 0.75
+		train = df[is_test==False]
+		validate = df[is_test==True]
+		
+		# Run Nearest Neighbors and Validate
+		clf = KNeighborsClassifier()
+		clf.fit(train[features], train[variable])
+		validate['predictions'] = clf.predict(validate[features])
 
-	def run_methods(methods):
-		'''
-		Takes a list of methods and outputs CSV file for each with predicted values
-		'''
-
-
+		# Create CSV of Predictions
+		validate.to_csv('predictions.csv')
+		
+		return accuracy_score(validate[variable], validate['predictions'])
 	
 
 
